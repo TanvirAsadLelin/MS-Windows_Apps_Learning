7b99859550d6bee801e00642c855014ca018faaa
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-02-23 22:48:56 +0600

    Creating calcualtor windows apps part 2

--- CreatingCalculator/CreatingCalculator_Windows_Apps/Form1.Designer.cs
+++ CreatingCalculator/CreatingCalculator_Windows_Apps/Form1.Designer.cs
@@ -28,12 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFirstNumber = new System.Windows.Forms.TextBox();
+            this.txtSecondNumber = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnDivision = new System.Windows.Forms.Button();
+            this.lblShowResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cooper Black", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(183, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(455, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "My Calculator Application";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(74, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(208, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter first number:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(45, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(237, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Enter second number:";
+            // 
+            // txtFirstNumber
+            // 
+            this.txtFirstNumber.Location = new System.Drawing.Point(288, 138);
+            this.txtFirstNumber.Multiline = true;
+            this.txtFirstNumber.Name = "txtFirstNumber";
+            this.txtFirstNumber.Size = new System.Drawing.Size(205, 34);
+            this.txtFirstNumber.TabIndex = 3;
+            // 
+            // txtSecondNumber
+            // 
+            this.txtSecondNumber.Location = new System.Drawing.Point(288, 198);
+            this.txtSecondNumber.Multiline = true;
+            this.txtSecondNumber.Name = "txtSecondNumber";
+            this.txtSecondNumber.Size = new System.Drawing.Size(205, 34);
+            this.txtSecondNumber.TabIndex = 4;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnAdd.Location = new System.Drawing.Point(74, 374);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(127, 55);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Addition";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSubtract.Location = new System.Drawing.Point(264, 374);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(127, 55);
+            this.btnSubtract.TabIndex = 6;
+            this.btnSubtract.Text = "Subtract";
+            this.btnSubtract.UseVisualStyleBackColor = true;
+            this.btnSubtract.Click += new System.EventHandler(this.btnSubtract_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnMultiply.Location = new System.Drawing.Point(458, 374);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(122, 55);
+            this.btnMultiply.TabIndex = 7;
+            this.btnMultiply.Text = "Multiply";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnDivision
+            // 
+            this.btnDivision.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnDivision.Location = new System.Drawing.Point(644, 374);
+            this.btnDivision.Name = "btnDivision";
+            this.btnDivision.Size = new System.Drawing.Size(107, 55);
+            this.btnDivision.TabIndex = 8;
+            this.btnDivision.Text = "Division";
+            this.btnDivision.UseVisualStyleBackColor = true;
+            this.btnDivision.Click += new System.EventHandler(this.btnDivision_Click);
+            // 
+            // lblShowResult
+            // 
+            this.lblShowResult.AutoSize = true;
+            this.lblShowResult.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblShowResult.Location = new System.Drawing.Point(341, 269);
+            this.lblShowResult.Name = "lblShowResult";
+            this.lblShowResult.Size = new System.Drawing.Size(70, 25);
+            this.lblShowResult.TabIndex = 9;
+            this.lblShowResult.Text = "label4";
+            this.lblShowResult.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(824, 522);
+            this.Controls.Add(this.lblShowResult);
+            this.Controls.Add(this.btnDivision);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtSecondNumber);
+            this.Controls.Add(this.txtFirstNumber);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "My calculator App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtFirstNumber;
+        private TextBox txtSecondNumber;
+        private Button btnAdd;
+        private Button btnSubtract;
+        private Button btnMultiply;
+        private Button btnDivision;
+        private Label lblShowResult;
     }
 }
--- CreatingCalculator/CreatingCalculator_Windows_Apps/Form1.cs
+++ CreatingCalculator/CreatingCalculator_Windows_Apps/Form1.cs
@@ -6,5 +6,101 @@ namespace CreatingCalculator_Windows_Apps
         {
             InitializeComponent();
         }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtFirstNumber.Text != "" && txtFirstNumber.Text != "")
+            {
+                int num1 = int.Parse(txtFirstNumber.Text);
+                int num2 = int.Parse(txtSecondNumber.Text);
+
+                int result = num1 + num2;
+
+                //  MessageBox.Show("The addition result is " + result.ToString());
+                lblShowResult.Text = "The result is " + result.ToString();
+                lblShowResult.Visible
+                    = true;
+
+            }
+
+            else
+            {
+                lblShowResult.Text = "Please insert two numbers";
+
+                lblShowResult.Visible = true;
+            }
+        }
+
+        private void btnSubtract_Click(object sender, EventArgs e)
+        {
+            if (txtFirstNumber.Text != "" && txtFirstNumber.Text != "")
+            {
+                int num1 = int.Parse(txtFirstNumber.Text);
+                int num2 = int.Parse(txtSecondNumber.Text);
+
+                int result = num1 - num2;
+
+                //  MessageBox.Show("The addition result is " + result.ToString());
+                lblShowResult.Text = "The result is " + result.ToString();
+                lblShowResult.Visible
+                    = true;
+
+            }
+
+            else
+            {
+                lblShowResult.Text = "Please insert two numbers";
+
+                lblShowResult.Visible = true;
+            }
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            if (txtFirstNumber.Text != "" && txtFirstNumber.Text != "")
+            {
+                int num1 = int.Parse(txtFirstNumber.Text);
+                int num2 = int.Parse(txtSecondNumber.Text);
+
+                int result = num1 * num2;
+
+                //  MessageBox.Show("The addition result is " + result.ToString());
+                lblShowResult.Text = "The result is " + result.ToString();
+                lblShowResult.Visible
+                    = true;
+
+            }
+
+            else
+            {
+                lblShowResult.Text = "Please insert two numbers";
+
+                lblShowResult.Visible = true;
+            }
+        }
+
+        private void btnDivision_Click(object sender, EventArgs e)
+        {
+            if (txtFirstNumber.Text != "" && txtFirstNumber.Text != "")
+            {
+                int num1 = int.Parse(txtFirstNumber.Text);
+                int num2 = int.Parse(txtSecondNumber.Text);
+
+                int result = num1 / num2;
+
+                //  MessageBox.Show("The addition result is " + result.ToString());
+                lblShowResult.Text = "The result is " + result.ToString();
+                lblShowResult.Visible
+                    = true;
+
+            }
+
+            else
+            {
+                lblShowResult.Text = "Please insert two numbers";
+
+                lblShowResult.Visible = true;
+            }
+        }
     }
 }
