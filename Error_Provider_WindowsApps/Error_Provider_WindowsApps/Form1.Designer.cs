dd0081caf84dfc020cedd99b2082c09dc931fb82
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-02-26 19:04:17 +0600

    Error provider with custom resources for windows apps

---
+++ Error_Provider_WindowsApps/Error_Provider_WindowsApps/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Error_Provider_WindowsApps
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxAge = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(90, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(177, 99);
+            this.textBoxName.Multiline = true;
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(214, 22);
+            this.textBoxName.TabIndex = 0;
+            this.textBoxName.Leave += new System.EventHandler(this.textBoxName_Leave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(103, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Age";
+            // 
+            // textBoxAge
+            // 
+            this.textBoxAge.Location = new System.Drawing.Point(177, 158);
+            this.textBoxAge.Multiline = true;
+            this.textBoxAge.Name = "textBoxAge";
+            this.textBoxAge.Size = new System.Drawing.Size(214, 22);
+            this.textBoxAge.TabIndex = 1;
+            this.textBoxAge.Leave += new System.EventHandler(this.textBoxAge_Leave);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(222, 237);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(113, 43);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // errorProvider2
+            // 
+            this.errorProvider2.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.textBoxAge);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Error Provider";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxAge;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ErrorProvider errorProvider2;
+    }
+}
+
---
+++ Error_Provider_WindowsApps/Error_Provider_WindowsApps/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Error_Provider_WindowsApps
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxName_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxName.Text))
+            {
+                textBoxName.Focus();
+
+                errorProvider1.Icon = Properties.Resources.error;
+                // MessageBox.Show("Please fill the name !!");
+
+                errorProvider1.SetError(this.textBoxName, "Please fill the name !!");
+            }
+            else
+            {
+                //  errorProvider1.Clear();
+                errorProvider1.Icon = Properties.Resources.check;
+            }
+                
+        }
+
+        private void textBoxAge_Leave(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(textBoxAge.Text))
+            {
+                textBoxAge.Focus();
+                errorProvider2.Icon = Properties.Resources.error;
+
+                //  MessageBox.Show("Please fill the age !!");
+
+                errorProvider2.SetError(this.textBoxAge, "Please fill the age !!");
+            }
+            else
+            {
+                // errorProvider2.Clear();
+                errorProvider2.Icon = Properties.Resources.check;
+            }
+
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(textBoxName.Text))
+            {
+                textBoxAge.Focus();
+                //  MessageBox.Show("Please fill the age !!");
+
+                errorProvider1.SetError(this.textBoxName, "Please fill the name !!");
+            }
+
+           else if (string.IsNullOrEmpty(textBoxAge.Text))
+            {
+                textBoxAge.Focus();
+                //  MessageBox.Show("Please fill the age !!");
+
+                errorProvider2.SetError(this.textBoxAge, "Please fill the age !!");
+            }
+            else
+            {
+                MessageBox.Show("Welcome");
+            }
+           
+        }
+    }
+}
