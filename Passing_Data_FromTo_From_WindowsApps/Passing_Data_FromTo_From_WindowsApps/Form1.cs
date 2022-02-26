56d01f88781bede643181b43a9adfb6ee71bae3d
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-02-26 09:19:48 +0600

    Passing data another form is completed for windows apps

--- Passing_Data_FromTo_From_WindowsApps/Passing_Data_FromTo_From_WindowsApps/Form1.cs
+++ Passing_Data_FromTo_From_WindowsApps/Passing_Data_FromTo_From_WindowsApps/Form1.cs
@@ -12,9 +12,37 @@ namespace Passing_Data_FromTo_From_WindowsApps
 {
     public partial class Form1 : Form
     {
+        public static string textFisrtName;
+        public static string textMiddleName;
+        public static string textLastName;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if(textBoxFirstname.Text != "" && textBoxMiddlename.Text != "" && textBoxLastName.Text != "" )
+            {
+                textFisrtName = textBoxFirstname.Text;
+                textMiddleName = textBoxMiddlename.Text;
+                textLastName = textBoxLastName.Text;
+
+                Form2 form2 = new Form2();
+                form2.Show();
+            }
+
+            else
+            {
+                MessageBox.Show("Please fill up three text box!");
+            }
+            
         }
     }
 }
---
+++ Passing_Data_FromTo_From_WindowsApps/Passing_Data_FromTo_From_WindowsApps/Form2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Passing_Data_FromTo_From_WindowsApps
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            //lblFirstName.Text = Form1.textFisrtName;
+            //lblMiddleName.Text = Form1.textMiddleName;
+            //lblLastName.Text = Form1.textLastName;
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblFirstName.Text = Form1.textFisrtName;
+            lblMiddleName.Text = Form1.textMiddleName;
+            lblLastName.Text = Form1.textLastName;
+
+            lblFirstName.Visible = true;
+            lblMiddleName.Visible = true;
+            lblLastName.Visible = true;
+        }
+    }
+}
