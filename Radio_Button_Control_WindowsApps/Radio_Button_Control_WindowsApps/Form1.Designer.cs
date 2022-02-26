5cea6d3f08e5b8bdbf7dde1c9ab22feb0c20e702
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-02-26 11:20:53 +0600

    Radio Button for windows apps

---
+++ Radio_Button_Control_WindowsApps/Radio_Button_Control_WindowsApps/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace Radio_Button_Control_WindowsApps
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButtonIndia = new System.Windows.Forms.RadioButton();
+            this.radioButtonCanada = new System.Windows.Forms.RadioButton();
+            this.radioButtonGer = new System.Windows.Forms.RadioButton();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButtonBlue = new System.Windows.Forms.RadioButton();
+            this.radioButtonYellow = new System.Windows.Forms.RadioButton();
+            this.radioButtonGreen = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnChangeColour = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButtonPurple = new System.Windows.Forms.RadioButton();
+            this.radioButtonBrown = new System.Windows.Forms.RadioButton();
+            this.radioButtonRed = new System.Windows.Forms.RadioButton();
+            this.radioButtonBangla = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select your country ";
+            // 
+            // radioButtonIndia
+            // 
+            this.radioButtonIndia.AutoSize = true;
+            this.radioButtonIndia.Location = new System.Drawing.Point(33, 133);
+            this.radioButtonIndia.Name = "radioButtonIndia";
+            this.radioButtonIndia.Size = new System.Drawing.Size(57, 20);
+            this.radioButtonIndia.TabIndex = 1;
+            this.radioButtonIndia.TabStop = true;
+            this.radioButtonIndia.Text = "India";
+            this.radioButtonIndia.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonCanada
+            // 
+            this.radioButtonCanada.AutoSize = true;
+            this.radioButtonCanada.Location = new System.Drawing.Point(33, 159);
+            this.radioButtonCanada.Name = "radioButtonCanada";
+            this.radioButtonCanada.Size = new System.Drawing.Size(76, 20);
+            this.radioButtonCanada.TabIndex = 1;
+            this.radioButtonCanada.TabStop = true;
+            this.radioButtonCanada.Text = "Canada";
+            this.radioButtonCanada.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonGer
+            // 
+            this.radioButtonGer.AutoSize = true;
+            this.radioButtonGer.Location = new System.Drawing.Point(33, 185);
+            this.radioButtonGer.Name = "radioButtonGer";
+            this.radioButtonGer.Size = new System.Drawing.Size(83, 20);
+            this.radioButtonGer.TabIndex = 1;
+            this.radioButtonGer.TabStop = true;
+            this.radioButtonGer.Text = "Germany";
+            this.radioButtonGer.UseVisualStyleBackColor = true;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(29, 235);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(105, 35);
+            this.btnSubmit.TabIndex = 0;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnChangeColour);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.radioButtonGreen);
+            this.groupBox1.Controls.Add(this.radioButtonYellow);
+            this.groupBox1.Controls.Add(this.radioButtonBlue);
+            this.groupBox1.Location = new System.Drawing.Point(244, 67);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(335, 220);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // radioButtonBlue
+            // 
+            this.radioButtonBlue.AutoSize = true;
+            this.radioButtonBlue.Location = new System.Drawing.Point(46, 70);
+            this.radioButtonBlue.Name = "radioButtonBlue";
+            this.radioButtonBlue.Size = new System.Drawing.Size(55, 20);
+            this.radioButtonBlue.TabIndex = 0;
+            this.radioButtonBlue.TabStop = true;
+            this.radioButtonBlue.Text = "Blue";
+            this.radioButtonBlue.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonYellow
+            // 
+            this.radioButtonYellow.AutoSize = true;
+            this.radioButtonYellow.Location = new System.Drawing.Point(46, 96);
+            this.radioButtonYellow.Name = "radioButtonYellow";
+            this.radioButtonYellow.Size = new System.Drawing.Size(68, 20);
+            this.radioButtonYellow.TabIndex = 0;
+            this.radioButtonYellow.TabStop = true;
+            this.radioButtonYellow.Text = "Yellow";
+            this.radioButtonYellow.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonGreen
+            // 
+            this.radioButtonGreen.AutoSize = true;
+            this.radioButtonGreen.Location = new System.Drawing.Point(46, 122);
+            this.radioButtonGreen.Name = "radioButtonGreen";
+            this.radioButtonGreen.Size = new System.Drawing.Size(65, 20);
+            this.radioButtonGreen.TabIndex = 0;
+            this.radioButtonGreen.TabStop = true;
+            this.radioButtonGreen.Text = "Green";
+            this.radioButtonGreen.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(30, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Select colour";
+            // 
+            // btnChangeColour
+            // 
+            this.btnChangeColour.Location = new System.Drawing.Point(46, 168);
+            this.btnChangeColour.Name = "btnChangeColour";
+            this.btnChangeColour.Size = new System.Drawing.Size(132, 23);
+            this.btnChangeColour.TabIndex = 4;
+            this.btnChangeColour.Text = "Change Colour";
+            this.btnChangeColour.UseVisualStyleBackColor = true;
+            this.btnChangeColour.Click += new System.EventHandler(this.btnChangeColour_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.radioButtonPurple);
+            this.groupBox2.Controls.Add(this.radioButtonBrown);
+            this.groupBox2.Controls.Add(this.radioButtonRed);
+            this.groupBox2.Location = new System.Drawing.Point(631, 67);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(335, 220);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 168);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Change Colour";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(30, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 23);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Select colour";
+            // 
+            // radioButtonPurple
+            // 
+            this.radioButtonPurple.AutoSize = true;
+            this.radioButtonPurple.Location = new System.Drawing.Point(46, 122);
+            this.radioButtonPurple.Name = "radioButtonPurple";
+            this.radioButtonPurple.Size = new System.Drawing.Size(67, 20);
+            this.radioButtonPurple.TabIndex = 0;
+            this.radioButtonPurple.TabStop = true;
+            this.radioButtonPurple.Text = "Purple";
+            this.radioButtonPurple.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonBrown
+            // 
+            this.radioButtonBrown.AutoSize = true;
+            this.radioButtonBrown.Location = new System.Drawing.Point(46, 96);
+            this.radioButtonBrown.Name = "radioButtonBrown";
+            this.radioButtonBrown.Size = new System.Drawing.Size(65, 20);
+            this.radioButtonBrown.TabIndex = 0;
+            this.radioButtonBrown.TabStop = true;
+            this.radioButtonBrown.Text = "Brown";
+            this.radioButtonBrown.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonRed
+            // 
+            this.radioButtonRed.AutoSize = true;
+            this.radioButtonRed.Location = new System.Drawing.Point(46, 70);
+            this.radioButtonRed.Name = "radioButtonRed";
+            this.radioButtonRed.Size = new System.Drawing.Size(54, 20);
+            this.radioButtonRed.TabIndex = 0;
+            this.radioButtonRed.TabStop = true;
+            this.radioButtonRed.Text = "Red";
+            this.radioButtonRed.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonBangla
+            // 
+            this.radioButtonBangla.AutoSize = true;
+            this.radioButtonBangla.Location = new System.Drawing.Point(33, 107);
+            this.radioButtonBangla.Name = "radioButtonBangla";
+            this.radioButtonBangla.Size = new System.Drawing.Size(101, 20);
+            this.radioButtonBangla.TabIndex = 1;
+            this.radioButtonBangla.TabStop = true;
+            this.radioButtonBangla.Text = "Bangladesh";
+            this.radioButtonBangla.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1056, 389);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.radioButtonGer);
+            this.Controls.Add(this.radioButtonCanada);
+            this.Controls.Add(this.radioButtonIndia);
+            this.Controls.Add(this.radioButtonBangla);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Radio Button";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButtonIndia;
+        private System.Windows.Forms.RadioButton radioButtonCanada;
+        private System.Windows.Forms.RadioButton radioButtonGer;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButtonGreen;
+        private System.Windows.Forms.RadioButton radioButtonYellow;
+        private System.Windows.Forms.RadioButton radioButtonBlue;
+        private System.Windows.Forms.Button btnChangeColour;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButtonPurple;
+        private System.Windows.Forms.RadioButton radioButtonBrown;
+        private System.Windows.Forms.RadioButton radioButtonRed;
+        private System.Windows.Forms.RadioButton radioButtonBangla;
+    }
+}
+
---
+++ Radio_Button_Control_WindowsApps/Radio_Button_Control_WindowsApps/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Radio_Button_Control_WindowsApps
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if(radioButtonBangla.Checked == true)
+            {
+                MessageBox.Show("You have selected: " + radioButtonBangla.Text);
+            }
+           else if (radioButtonCanada.Checked == true)
+            {
+                MessageBox.Show("You have selected: " + radioButtonCanada.Text);
+            }
+
+            else if (radioButtonIndia.Checked == true)
+            {
+                MessageBox.Show("You have selected: " + radioButtonIndia.Text);
+            }
+
+            else if (radioButtonGer.Checked == true)
+            {
+                MessageBox.Show("You have selected: " + radioButtonGer.Text);
+            }
+
+            else
+            {
+                MessageBox.Show("Please select your country");
+            }
+
+        }
+
+        private void btnChangeColour_Click(object sender, EventArgs e)
+        {
+            if (radioButtonBlue.Checked == true)
+            {
+                groupBox1.BackColor = Color.Blue;
+            }
+            else if (radioButtonYellow.Checked == true)
+            {
+                groupBox1.BackColor = Color.Yellow;
+            }
+
+            else if (radioButtonGreen.Checked == true)
+            {
+                groupBox1.BackColor = Color.Green;
+            }
+
+            else
+            {
+                MessageBox.Show("Please select a colour");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButtonRed.Checked == true)
+            {
+                this.BackColor = System.Drawing.Color.Red;
+            }
+            else if (radioButtonBrown.Checked == true)
+            {
+                this.BackColor = System.Drawing.Color.Brown;
+            }
+
+            else if (radioButtonPurple.Checked == true) 
+            {
+                this.BackColor = System.Drawing.Color.Purple;
+            }
+
+            else
+            {
+                MessageBox.Show("Please select a colour");
+            }
+        }
+    }
+}
