edd9dbe616a08bb6f813b14b2571fda3e8bb88be
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-02-27 20:19:29 +0600

    Set fixed time & running time for windows apps

---
+++ SetFixedAndRunningTime_WindowsApps/SetFixedAndRunningTime_WindowsApps/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace SetFixedAndRunningTime_WindowsApps
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblNowTime = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNowTime
+            // 
+            this.lblNowTime.AutoSize = true;
+            this.lblNowTime.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNowTime.Location = new System.Drawing.Point(296, 63);
+            this.lblNowTime.Name = "lblNowTime";
+            this.lblNowTime.Size = new System.Drawing.Size(86, 32);
+            this.lblNowTime.TabIndex = 0;
+            this.lblNowTime.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(298, 202);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(138, 66);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.lblNowTime);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNowTime;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ SetFixedAndRunningTime_WindowsApps/SetFixedAndRunningTime_WindowsApps/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SetFixedAndRunningTime_WindowsApps
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //DateTime dt = DateTime.Now;
+            //lblNowTime.Text = dt.ToString();
+           // lblNowTime.Text = dt.ToLongDateString();
+          //  lblNowTime.Text = dt.ToShortDateString();
+         //   lblNowTime.Text = dt.ToLongTimeString();
+
+            timer1.Start();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            lblNowTime.Text = dt.ToString();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+    }
+}
