902b250e6c61e66113e61f8e83764982eeea4483
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-02-27 14:45:16 +0600

    Placeholder set in TextBox for windows apps

---
+++ PlaceholderSet_Textbox_WindowsApps/PlaceholderSet_Textbox_WindowsApps/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace PlaceholderSet_Textbox_WindowsApps
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.ForeColor = System.Drawing.Color.Gray;
+            this.textBoxName.Location = new System.Drawing.Point(120, 109);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(287, 22);
+            this.textBoxName.TabIndex = 0;
+            this.textBoxName.Text = "Enter your name";
+            this.textBoxName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxName.Enter += new System.EventHandler(this.textBoxName_Enter);
+            this.textBoxName.Leave += new System.EventHandler(this.textBoxName_Leave);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(232, 255);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 1;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.ForeColor = System.Drawing.Color.Gray;
+            this.textBoxEmail.Location = new System.Drawing.Point(120, 175);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(287, 22);
+            this.textBoxEmail.TabIndex = 0;
+            this.textBoxEmail.Text = "Enter your email";
+            this.textBoxEmail.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxEmail.Enter += new System.EventHandler(this.textBoxEmail_Enter);
+            this.textBoxEmail.Leave += new System.EventHandler(this.textBoxEmail_Leave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 450);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox textBoxEmail;
+    }
+}
+
---
+++ PlaceholderSet_Textbox_WindowsApps/PlaceholderSet_Textbox_WindowsApps/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlaceholderSet_Textbox_WindowsApps
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxName_Enter(object sender, EventArgs e)
+        {
+            if(textBoxName.Text == "Enter your name")
+            {
+                textBoxName.Text = "";
+                textBoxName.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBoxName_Leave(object sender, EventArgs e)
+        {
+            if (textBoxName.Text == "")
+            {
+                textBoxName.Text = "Enter your name";
+                textBoxName.ForeColor = Color.Gray;
+            }
+        }
+
+        private void textBoxEmail_Enter(object sender, EventArgs e)
+        {
+            if (textBoxEmail.Text == "Enter your email")
+            {
+                textBoxEmail.Text = "";
+                textBoxEmail.ForeColor = Color.Black;
+            }
+
+        }
+
+        private void textBoxEmail_Leave(object sender, EventArgs e)
+        {
+            if (textBoxEmail.Text == "") 
+            {
+                textBoxEmail.Text = "Enter your email";
+                textBoxEmail.ForeColor = Color.Gray;
+            }
+
+        }
+    }
+}
