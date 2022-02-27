1bf04cbed007ab2a923db1056ee59ee2a2f86420
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-02-27 09:50:29 +0600

    Modified Email textbox validation and Password and confirm password validation for windows apps

---
+++ PasswordAndConfirmPassword_Validation_WindowsApps/PasswordAndConfirmPassword_Validation_WindowsApps/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace PasswordAndConfirmPassword_Validation_WindowsApps
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxUserName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxPass = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxConfirmPass = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.errorProviderUserName = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProviderPass = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProviderConfirmPass = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderUserName)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderPass)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderConfirmPass)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(90, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // textBoxUserName
+            // 
+            this.textBoxUserName.Location = new System.Drawing.Point(228, 117);
+            this.textBoxUserName.Name = "textBoxUserName";
+            this.textBoxUserName.Size = new System.Drawing.Size(292, 22);
+            this.textBoxUserName.TabIndex = 0;
+            this.textBoxUserName.Leave += new System.EventHandler(this.textBoxUserName_Leave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(90, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Password";
+            // 
+            // textBoxPass
+            // 
+            this.textBoxPass.Location = new System.Drawing.Point(227, 169);
+            this.textBoxPass.Name = "textBoxPass";
+            this.textBoxPass.Size = new System.Drawing.Size(292, 22);
+            this.textBoxPass.TabIndex = 1;
+            this.textBoxPass.Leave += new System.EventHandler(this.textBoxPass_Leave);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 217);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(197, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Confirm password";
+            // 
+            // textBoxConfirmPass
+            // 
+            this.textBoxConfirmPass.Location = new System.Drawing.Point(226, 220);
+            this.textBoxConfirmPass.Name = "textBoxConfirmPass";
+            this.textBoxConfirmPass.Size = new System.Drawing.Size(292, 22);
+            this.textBoxConfirmPass.TabIndex = 2;
+            this.textBoxConfirmPass.Leave += new System.EventHandler(this.textBoxConfirmPass_Leave);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(243, 289);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(145, 49);
+            this.btnSubmit.TabIndex = 3;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // errorProviderUserName
+            // 
+            this.errorProviderUserName.ContainerControl = this;
+            // 
+            // errorProviderPass
+            // 
+            this.errorProviderPass.ContainerControl = this;
+            // 
+            // errorProviderConfirmPass
+            // 
+            this.errorProviderConfirmPass.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 450);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.textBoxConfirmPass);
+            this.Controls.Add(this.textBoxPass);
+            this.Controls.Add(this.textBoxUserName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderUserName)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderPass)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderConfirmPass)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxUserName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxPass;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxConfirmPass;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.ErrorProvider errorProviderUserName;
+        private System.Windows.Forms.ErrorProvider errorProviderPass;
+        private System.Windows.Forms.ErrorProvider errorProviderConfirmPass;
+    }
+}
+
---
+++ PasswordAndConfirmPassword_Validation_WindowsApps/PasswordAndConfirmPassword_Validation_WindowsApps/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace PasswordAndConfirmPassword_Validation_WindowsApps
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Verbatim literal @ 
+        //  string pattern = "\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z";
+        string pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$";
+
+        private void textBoxUserName_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxUserName.Text) == true)
+            {
+                textBoxUserName.Focus();
+                errorProviderUserName.SetError(this.textBoxUserName, "Please enter username!");
+            }
+            else
+            {
+                errorProviderUserName.Clear();
+            }
+        }
+
+        private void textBoxPass_Leave(object sender, EventArgs e)
+        {
+            if(Regex.IsMatch(textBoxPass.Text, pattern) == false)
+            {
+                textBoxPass.Focus();
+                errorProviderPass.SetError(this.textBoxPass, "Please enter Uppercase , Lowercase, Numbers , Special characters");
+
+            }
+            else
+            {
+                errorProviderPass.Clear();
+            }
+
+        }
+
+        private void textBoxConfirmPass_Leave(object sender, EventArgs e)
+        {
+           if(textBoxPass.Text != textBoxConfirmPass.Text)
+            {
+                textBoxConfirmPass.Focus();
+                errorProviderConfirmPass.SetError(this.textBoxPass, "Password is not match!");
+            }
+
+            else
+            {
+                  errorProviderConfirmPass.Clear();
+            }
+
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxUserName.Text) == true)
+            {
+                textBoxUserName.Focus();
+                errorProviderUserName.SetError(this.textBoxUserName, "Please enter username!");
+            }
+
+            else if (Regex.IsMatch(textBoxPass.Text, pattern) == false)
+            {
+                textBoxPass.Focus();
+                errorProviderPass.SetError(this.textBoxPass, "Please enter Uppercase , Lowercase, Numbers , Special characters");
+
+            }
+
+            else if (textBoxPass.Text != textBoxConfirmPass.Text)
+            {
+                textBoxConfirmPass.Focus();
+                errorProviderConfirmPass.SetError(this.textBoxPass, "Password is not match!");
+            }
+
+            else
+            {
+                MessageBox.Show("Welcome","Success",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+        }
+    }
+}
