07c35fa0493899270aab4cc4d63e33135d7733de
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-02-28 12:22:37 +0600

    Create login form and sql server connection with login for windows apps

---
+++ LoginForm_SQLServerDB_WindowsApps/LoginForm_SQLServerDB_WindowsApps/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace LoginForm_SQLServerDB_WindowsApps
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBoxSignIn = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.checkBoxShowPass = new System.Windows.Forms.CheckBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.errorProviderUsername = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProviderPass = new System.Windows.Forms.ErrorProvider(this.components);
+            this.groupBoxSignIn.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderUsername)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderPass)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(252, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login Form";
+            // 
+            // groupBoxSignIn
+            // 
+            this.groupBoxSignIn.Controls.Add(this.btnLogin);
+            this.groupBoxSignIn.Controls.Add(this.checkBoxShowPass);
+            this.groupBoxSignIn.Controls.Add(this.textBoxPassword);
+            this.groupBoxSignIn.Controls.Add(this.textBoxUsername);
+            this.groupBoxSignIn.Controls.Add(this.label3);
+            this.groupBoxSignIn.Controls.Add(this.label2);
+            this.groupBoxSignIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxSignIn.Location = new System.Drawing.Point(301, 93);
+            this.groupBoxSignIn.Name = "groupBoxSignIn";
+            this.groupBoxSignIn.Size = new System.Drawing.Size(395, 297);
+            this.groupBoxSignIn.TabIndex = 1;
+            this.groupBoxSignIn.TabStop = false;
+            this.groupBoxSignIn.Text = "Sign In";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Username: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Password:";
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxUsername.Location = new System.Drawing.Point(135, 49);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(219, 27);
+            this.textBoxUsername.TabIndex = 0;
+            this.textBoxUsername.TextChanged += new System.EventHandler(this.textBoxUsername_TextChanged);
+            this.textBoxUsername.Leave += new System.EventHandler(this.textBoxUsername_Leave);
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPassword.Location = new System.Drawing.Point(134, 112);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(219, 27);
+            this.textBoxPassword.TabIndex = 1;
+            this.textBoxPassword.UseSystemPasswordChar = true;
+            this.textBoxPassword.TextChanged += new System.EventHandler(this.textBoxPassword_TextChanged);
+            this.textBoxPassword.Leave += new System.EventHandler(this.textBoxPassword_Leave);
+            // 
+            // checkBoxShowPass
+            // 
+            this.checkBoxShowPass.AutoSize = true;
+            this.checkBoxShowPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxShowPass.Location = new System.Drawing.Point(135, 163);
+            this.checkBoxShowPass.Name = "checkBoxShowPass";
+            this.checkBoxShowPass.Size = new System.Drawing.Size(149, 24);
+            this.checkBoxShowPass.TabIndex = 2;
+            this.checkBoxShowPass.Text = "Show password";
+            this.checkBoxShowPass.UseVisualStyleBackColor = true;
+            this.checkBoxShowPass.CheckedChanged += new System.EventHandler(this.checkBoxShowPass_CheckedChanged);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Image = ((System.Drawing.Image)(resources.GetObject("btnLogin.Image")));
+            this.btnLogin.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLogin.Location = new System.Drawing.Point(0, 212);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(363, 58);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(24, 105);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(237, 264);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // errorProviderUsername
+            // 
+            this.errorProviderUsername.ContainerControl = this;
+            // 
+            // errorProviderPass
+            // 
+            this.errorProviderPass.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 402);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBoxSignIn);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.groupBoxSignIn.ResumeLayout(false);
+            this.groupBoxSignIn.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderUsername)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderPass)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBoxSignIn;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.CheckBox checkBoxShowPass;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ErrorProvider errorProviderUsername;
+        private System.Windows.Forms.ErrorProvider errorProviderPass;
+    }
+}
+
---
+++ LoginForm_SQLServerDB_WindowsApps/LoginForm_SQLServerDB_WindowsApps/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace LoginForm_SQLServerDB_WindowsApps
+{
+    public partial class Form1 : Form
+    {
+        string cs = ConfigurationManager.ConnectionStrings["DBConnectionString"].ConnectionString;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBoxShowPass_CheckedChanged(object sender, EventArgs e)
+        {
+            bool check = checkBoxShowPass.Checked;
+            switch(check)
+            {
+                case true:
+                    textBoxPassword.UseSystemPasswordChar = false;
+                    break;
+                default:
+                    textBoxPassword.UseSystemPasswordChar = true;
+                    break;
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (textBoxUsername.Text != "" && textBoxPassword.Text!="")
+            {
+                SqlConnection con = new SqlConnection(cs);
+
+                string query = "select * from Login_tbl where username = @user and password = @pass";
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                cmd.Parameters.AddWithValue("@user", textBoxUsername.Text);
+                cmd.Parameters.AddWithValue("@pass", textBoxPassword.Text);
+
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows == true)
+                {
+                    MessageBox.Show("Login Successfull", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Login fail", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                con.Close();
+
+            }
+            else
+            {
+                MessageBox.Show("Please fill both field", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            
+            
+        }
+
+        private void textBoxUsername_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBoxPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxUsername_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxUsername.Text) == true)
+            {
+                textBoxUsername.Focus();
+                errorProviderUsername.SetError(this.textBoxUsername, "Please fill this filed!");
+
+            }
+            else
+            {
+                errorProviderUsername.Clear();
+            }
+        }
+
+        private void textBoxPassword_Leave(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(textBoxPassword.Text) == true)
+            {
+                textBoxPassword.Focus();
+                errorProviderPass.SetError(this.textBoxPassword, "Please fill this field!");
+            }
+            else
+            {
+                errorProviderPass.Clear();
+            }
+        }
+    }
+}
