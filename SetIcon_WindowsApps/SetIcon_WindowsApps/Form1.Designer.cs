7a1ffcc53c6b65249e07a71e15b5014461868930
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-03-01 15:27:07 +0600

    Edit Check Box Control And Set Icon in Apps and All form

---
+++ SetIcon_WindowsApps/SetIcon_WindowsApps/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace SetIcon_WindowsApps
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1000 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.btnTotal = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblIndex = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkBox1000
+            // 
+            this.checkBox1000.AutoSize = true;
+            this.checkBox1000.Location = new System.Drawing.Point(113, 80);
+            this.checkBox1000.Name = "checkBox1000";
+            this.checkBox1000.Size = new System.Drawing.Size(57, 20);
+            this.checkBox1000.TabIndex = 0;
+            this.checkBox1000.Text = "1000";
+            this.checkBox1000.UseVisualStyleBackColor = true;
+            this.checkBox1000.CheckedChanged += new System.EventHandler(this.checkBox1000_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(202, 80);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(57, 20);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "1000";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(419, 80);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(57, 20);
+            this.checkBox2.TabIndex = 0;
+            this.checkBox2.Text = "1000";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(309, 80);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(57, 20);
+            this.checkBox3.TabIndex = 0;
+            this.checkBox3.Text = "1000";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(540, 80);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(57, 20);
+            this.checkBox4.TabIndex = 0;
+            this.checkBox4.Text = "1000";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // btnTotal
+            // 
+            this.btnTotal.Location = new System.Drawing.Point(282, 149);
+            this.btnTotal.Name = "btnTotal";
+            this.btnTotal.Size = new System.Drawing.Size(95, 33);
+            this.btnTotal.TabIndex = 1;
+            this.btnTotal.Text = "Get Value";
+            this.btnTotal.UseVisualStyleBackColor = true;
+            this.btnTotal.Click += new System.EventHandler(this.btnTotal_Click);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(303, 226);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(86, 32);
+            this.lblTotal.TabIndex = 2;
+            this.lblTotal.Text = "label1";
+            this.lblTotal.Visible = false;
+            // 
+            // lblIndex
+            // 
+            this.lblIndex.AutoSize = true;
+            this.lblIndex.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIndex.Location = new System.Drawing.Point(303, 278);
+            this.lblIndex.Name = "lblIndex";
+            this.lblIndex.Size = new System.Drawing.Size(86, 32);
+            this.lblIndex.TabIndex = 2;
+            this.lblIndex.Text = "label1";
+            this.lblIndex.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 367);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.checkBox1000);
+            this.Controls.Add(this.lblIndex);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.btnTotal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1000;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Button btnTotal;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblIndex;
+    }
+}
+
---
+++ SetIcon_WindowsApps/SetIcon_WindowsApps/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace SetIcon_WindowsApps
+{
+    public partial class Form1 : TemplateForm
+    {
+        string text1 = "";
+        string text2 = "";
+        string text3 = "";
+        string text4 = "";
+        string text5 = "";
+     
+
+        
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int index = 0;
+        private void checkBox1000_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1000.Checked == true)
+            {
+                text1 = checkBox1000.Text;
+                index = index + 1;
+              
+            }
+
+            else
+            {
+                text1 = "";
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                text2 = checkBox1.Text;
+                index = index + 1;
+
+            }
+
+            else
+            {
+                text2 = "";
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked == true)
+            {
+                text3 = checkBox2.Text;
+                index = index + 1;
+
+            }
+
+            else
+            {
+                text3 = "";
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked == true)
+            {
+                text4 = checkBox3.Text;
+                index = index + 1;
+
+            }
+
+            else
+            {
+                text4 = "";
+            }
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox4.Checked == true)
+            {
+                text5 = checkBox4.Text;
+                index = index + 1;
+
+            }
+
+            else
+            {
+                text5="";
+
+                
+            }
+        }
+
+        private void btnTotal_Click(object sender, EventArgs e)
+        {
+
+            int total = 0;
+            try
+            {
+                total = total + int.Parse(text1) + int.Parse(text2) + int.Parse(text3) + int.Parse(text4) + int.Parse(text5);
+                lblTotal.Text = "Total value : " + total.ToString();
+                lblTotal.Visible = true;
+
+            }
+            catch
+            {
+
+            }
+
+
+
+            lblIndex.Text ="Total select box: "+ index.ToString();
+            lblIndex.Visible = true;
+            
+
+
+
+        }
+    }
+}
