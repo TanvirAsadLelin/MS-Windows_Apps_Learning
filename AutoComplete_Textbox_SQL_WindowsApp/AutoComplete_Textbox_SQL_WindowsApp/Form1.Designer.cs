3da79ced9154f8281096ceeb67d391f32b41114b
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-03-02 18:35:45 +0600

    Auto complete textbox Array or String

---
+++ AutoComplete_Textbox_SQL_WindowsApp/AutoComplete_Textbox_SQL_WindowsApp/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace AutoComplete_Textbox_SQL_WindowsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxHobbies = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxHobbies
+            // 
+            this.textBoxHobbies.Location = new System.Drawing.Point(167, 142);
+            this.textBoxHobbies.Name = "textBoxHobbies";
+            this.textBoxHobbies.Size = new System.Drawing.Size(252, 22);
+            this.textBoxHobbies.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(164, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hobbies";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxHobbies);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxHobbies;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ AutoComplete_Textbox_SQL_WindowsApp/AutoComplete_Textbox_SQL_WindowsApp/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace AutoComplete_Textbox_SQL_WindowsApp
+{
+    public partial class Form1 : Form
+    {
+        string cs = ConfigurationManager.ConnectionStrings["DBConnectionstring"].ConnectionString;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void AutoCompleteTBArray()
+        {
+            string[] hobbies = { "Swimming", "Reading", "Walking", "Sleeping", "Eating" };
+            textBoxHobbies.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            // textBoxHobbies.AutoCompleteMode = AutoCompleteMode.Append;
+            // textBoxHobbies.AutoCompleteMode = AutoCompleteMode.Suggest;
+            textBoxHobbies.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            textBoxHobbies.AutoCompleteCustomSource.AddRange(hobbies);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           // AutoCompleteTBArray();
+
+            AutoCompleteTB_SQL();
+
+        }
+
+        void AutoCompleteTB_SQL()
+        {
+            SqlConnection con = new SqlConnection(cs);
+            string query = "select * from Hobbies_Tbl";
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            con.Open();
+            AutoCompleteStringCollection coll = new AutoCompleteStringCollection();
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                coll.Add(dr.GetString(0));
+
+            }
+            textBoxHobbies.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            textBoxHobbies.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            textBoxHobbies.AutoCompleteCustomSource = coll;
+
+            con.Close();
+        }
+    }
+}
